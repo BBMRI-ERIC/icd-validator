c2769da353bc8adc46189aa15c7f967d49364d41
Radovan Tomasik <tomasik.radovan@gmail.com>
2025-06-17 13:51:17 +0200

    Reformat the code

--- src/main/java/eu/bbmri_eric/App.java
+++
@@ -1,8 +0,0 @@
-package eu.bbmri_eric;
-
-public class App {
-    public static void main(String[] args) {
-        ICDValidator validator = new ICD10Validator();
-        validator.isValid("B18.0");
-    }
-}
--- src/main/java/eu/bbmri_eric/ICDValidator.java
+++ src/main/java/eu/bbmri_eric/ICDValidator.java
@@ -1,13 +1,12 @@
 package eu.bbmri_eric;
 
-/**
- * A simple validator of ICD codes.
- */
+/** A simple validator of ICD codes. */
 public interface ICDValidator {
-    /**
-     * Check weather a given string is a valid ICD code
-     * @param icd code
-     * @return true if such an ICD code exists
-     */
-    boolean isValid(String icd);
+  /**
+   * Check weather a given string is a valid ICD code
+   *
+   * @param icd code
+   * @return true if such an ICD code exists
+   */
+  boolean isValid(String icd);
 }
